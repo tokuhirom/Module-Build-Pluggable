f23ca9ff5974d6541137c88eab5827ea5ba2490b
tokuhirom <tokuhirom@gmail.com>
2012-11-02 09:47:50 +0900

    added 'add_action' method

--- lib/Module/Build/Pluggable/Plugin.pm
+++ lib/Module/Build/Pluggable/Plugin.pm
@@ -14,18 +14,35 @@ sub new {
     bless { %args }, $class;
 }
 
-sub add_before_action_modifier {
-    my ($self, $target, $code) = @_;
+sub builder_class {
+    my $self = shift;
     my $builder = $self->builder;
        $builder = ref $builder if ref $builder;
+    return $builder;
+}
+
+# build
+sub add_before_action_modifier {
+    my ($self, $target, $code) = @_;
+    my $builder = $self->builder_class;
     install_modifier($builder, 'before', "ACTION_$target", $code);
 }
 
+# build
+sub add_action {
+    my ($self, $name, $code) = @_;
+    my $builder = $self->builder_class;
+    no strict 'refs';
+    *{"$builder\::ACTION_$name"} = $code;
+}
+
+# configure
 sub build_requires {
     my $self = shift;
     Module::Build::Pluggable::Util->add_prereqs($self->builder, 'build_requires', @_);
 }
 
+# configure
 sub configure_requires {
     my $self = shift;
     Module::Build::Pluggable::Util->add_prereqs($self->builder, 'configure_requires', @_);
--- lib/Module/Build/Pluggable/Plugin/PPPort.pm
+++ lib/Module/Build/Pluggable/Plugin/PPPort.pm
@@ -22,6 +22,10 @@ sub HOOK_build {
     my $filename = $self->filename || 'ppport.h';
     $self->add_before_action_modifier('build', sub {
         my $self = shift;
+        $self->depends_on('ppport_h');
+    });
+    $self->add_action('ppport_h', sub {
+        my $self = shift;
         unless (-e $filename) {
             $self->log_info("Writing $filename\n");
             Devel::PPPort::WriteFile($filename);
