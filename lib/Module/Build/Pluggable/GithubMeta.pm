92faa4add82be7f0615132d981643a4525e6c398
tokuhirom <tokuhirom@gmail.com>
2013-03-11 18:59:48 +0900

    added docs for Module::Build::Pluggable::GithubMeta

--- lib/Module/Build/Pluggable/GithubMeta.pm
+++ lib/Module/Build/Pluggable/GithubMeta.pm
@@ -47,4 +47,43 @@ sub _under_git {
 }
 
 1;
+__END__
 
+=head1 NAME
+
+Module::Build::Pluggable::GithubMeta - A Module::Build extension to include GitHub meta information in META.yml
+
+
+=head1 SYNOPSIS
+
+    use Module::Build::Pluggable qw(
+        GithubMeta
+    );
+
+    my $builder = Module::Build::Pluggable->new(
+        ...
+    );
+    $builder->crate_build_script();
+
+=head1 DESCRIPTION
+
+Module::Build::GithubMeta is a Module::Build extension to include GitHub http://github.com meta information in META.yml.
+
+It automatically detects if the distribution directory is under git version control and whether the origin is a GitHub repository and will set the repository and homepage meta in META.yml to the appropriate URLs for GitHub.
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno
+
+Based on code from L<Module::Install::GithubMeta> by Chris C<BinGOs> Williams, Based on code from L<Module::Install::Repository> by Tatsuhiko Miyagawa
+
+
+=head1 LICENSE
+
+Copyright E<copy> Tokuhiro Matsuno, Chris Williams and Tatsuhiko Miyagawa
+ 
+This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
+
+=head1 SEE ALSO
+
+L<Module::Build::Pluggable>
